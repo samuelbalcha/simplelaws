1731773351acc483d4b5540522805ebb868871c9
samuelbalcha <samuel.balcha@gmail.com>
2014-03-11 19:45:04 -0500

    updated version 2 based on comment

---
+++ public/javascripts/util.js
@@ -0,0 +1,56 @@
+/**
+ * Created by sammyb on 3/11/14.
+ */
+$(document).ready(function() {
+
+    $('.carousel').carousel({interval: 7000});
+
+    /* Source:
+     http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links/
+     */
+
+    function filterPath(string) {
+        return string
+            .replace(/^\//,'')
+            .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+            .replace(/\/$/,'');
+    }
+    var locationPath = filterPath(location.pathname);
+    var scrollElem = scrollableElement('html', 'body');
+
+    $('a[href*=#]').each(function() {
+        var thisPath = filterPath(this.pathname) || locationPath;
+        if (  locationPath == thisPath
+            && (location.hostname == this.hostname || !this.hostname)
+            && this.hash.replace(/#/,'') ) {
+            var $target = $(this.hash), target = this.hash;
+            if (target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function(event) {
+                    event.preventDefault();
+                    $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+                        location.hash = target;
+                    });
+                });
+            }
+        }
+    });
+
+    function scrollableElement(els) {
+        for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+            var el = arguments[i],
+                $scrollElement = $(el);
+            if ($scrollElement.scrollTop()> 0) {
+                return el;
+            } else {
+                $scrollElement.scrollTop(1);
+                var isScrollable = $scrollElement.scrollTop()> 0;
+                $scrollElement.scrollTop(0);
+                if (isScrollable) {
+                    return el;
+                }
+            }
+        }
+        return [];
+    }
+});
